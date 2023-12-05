4abe2a541a9c0e0cfee624aa7f6b44a92f86f580
Sean McElwain <semcelwa@gmail.com>
2023-12-04 21:31:04 -0600

    got part 2

--- day_2/main.py
+++ day_2/main.py
@@ -9,7 +9,7 @@ EXPECTED_RESULTS = {"red": 12, "green": 13, "blue": 14}
 
 def is_valid_game(sub_game: str) -> bool:
     for result in sub_game.split(","):
-        number, color = result.strip().split(" ")
+        number, color = result.strip().split()
         if EXPECTED_RESULTS[color] < int(number):
             return False
     return True
@@ -28,10 +28,27 @@ def part1():
         print(total)
 
 
+def get_minimum_cubes(game: str) -> int:
+    max_red = 0
+    max_blue = 0
+    max_green = 0
+    for sub_game in game.split(";"):
+        for result in sub_game.split(","):
+            number, color = result.strip().split()
+            match (color):
+                case "red":
+                    max_red = max(max_red, int(number))
+                case "blue":
+                    max_blue = max(max_blue, int(number))
+                case "green":
+                    max_green = max(max_green, int(number))
+    return max_red * max_green * max_blue
+
+
 @app.command()
 def part2():
     with (Path().cwd() / "day_2" / "day_2_input.txt").open() as fin:
-        print(fin.read())
+        print(sum(get_minimum_cubes(game.split(":")[1]) for game in fin))
 
 
 if __name__ == "__main__":
