bf2d034dac630fb7012140c41547041fb2ebba89
Sean McElwain <semcelwa@gmail.com>
2023-12-04 21:22:24 -0600

    got part 1 to work

--- day_2/main.py
+++ day_2/main.py
@@ -4,16 +4,33 @@ import typer
 
 app = typer.Typer()
 
+EXPECTED_RESULTS = {"red": 12, "green": 13, "blue": 14}
+
+
+def is_valid_game(sub_game: str) -> bool:
+    for result in sub_game.split(","):
+        number, color = result.strip().split(" ")
+        if EXPECTED_RESULTS[color] < int(number):
+            return False
+    return True
+
 
 @app.command()
 def part1():
-    with Path().cwd() / "day_2" / "day_2_part1_input.txt" as fin:
-        print(fin.read())
+    with (Path().cwd() / "day_2" / "day_2_input.txt").open() as fin:
+        total = sum(
+            game_id + 1
+            for game_id, game in enumerate(fin)
+            if all(
+                is_valid_game(sub_game) for sub_game in game.split(":")[1].split(";")
+            )
+        )
+        print(total)
 
 
 @app.command()
 def part2():
-    with Path().cwd() / "day_2" / "day_2_part2_input.txt" as fin:
+    with (Path().cwd() / "day_2" / "day_2_input.txt").open() as fin:
         print(fin.read())
 
 
