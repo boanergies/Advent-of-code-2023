f259538a4dc6e1ad73352865a842effc3182ff5c
Sean McElwain <semcelwa@gmail.com>
2023-12-04 20:49:11 -0600

    update tempalte

--- cookiecutter-advent-day/{{ cookiecutter.project_slug }}/main.py
+++ cookiecutter-advent-day/{{ cookiecutter.project_slug }}/main.py
@@ -7,13 +7,21 @@ app = typer.Typer()
 
 @app.command()
 def part1():
-    with Path().cwd() / "{{ cookiecutter.project_slug }}" / "{{ cookiecutter.project_slug }}_part1_input.txt" as fin:
+    with (
+        Path().cwd()
+        / "{{ cookiecutter.project_slug }}"
+        / "{{ cookiecutter.project_slug }}_part1_input.txt"
+    ).open() as fin:
         print(fin.read())
 
 
 @app.command()
 def part2():
-    with Path().cwd() / "{{ cookiecutter.project_slug }}" / "{{ cookiecutter.project_slug }}_part2_input.txt" as fin:
+    with (
+        Path().cwd()
+        / "{{ cookiecutter.project_slug }}"
+        / "{{ cookiecutter.project_slug }}_part2_input.txt"
+    ).open() as fin:
         print(fin.read())
 
 
